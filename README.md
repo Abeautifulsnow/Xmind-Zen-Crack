e01055bc0c8239cc7083d9421a78a4636b44f2ee
Abeautifulsnow <1491444340@qq.com>
2019-10-02 14:17:39 +0800

    first submit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
+<<<<<<< HEAD
 # Xmind-Zen-Crack
 Xmind Zen破解
+=======
+# Xmind_Zen_Crack
+Xmind Zen本地破解脚本
+## 说明
+仅作为个人学习使用，请勿用于任何商业用途。
+## 功能简介
+ * 去除软件右上角激活按钮
+ * 去除导出时激活弹窗
+ * 去除导出PDF文件时的水印
+ * 去除导出PNG文件时的水印
+## 使用说明
+将main.py和crack.py复制到Xmind Zen的安装目录下，然后通过python3 main.py运行该脚本即可。
+>>>>>>> first submit
---
+++ crack.py
@@ -0,0 +1,31 @@
+# coding=utf-8
+
+
+class Crack(object):
+    def __init__(self, f_name):
+        self.f_name = f_name
+
+    # 替换掉str
+    def replace_str(self, old, new):
+        with open(self.f_name) as f1:
+            content = f1.read()
+            f1.close()
+        rep = content.replace(old, new)
+        with open(self.f_name, "wb") as f2:
+            f2.write(rep.encode())
+            f2.close()
+
+    # 添加css
+    def add_css(self, k_word, new_css):
+        with open(self.f_name) as f2:
+            content = f2.read()
+            pos = content.find(k_word)
+            if pos != -1:
+                content = content[:pos+len(k_word)] + new_css + content[pos+len(k_word):]
+                f2 = open(self.f_name, "wb")
+                f2.write(content.encode())
+            f2.close()
+
+    # 删除所有文件
+    def del_all(self):
+        open(self.f_name, "wb").close()
